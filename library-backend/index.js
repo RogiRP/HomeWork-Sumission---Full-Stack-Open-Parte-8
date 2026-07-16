55882faad3eaba97b307d72a6959fe18b3529139
roger_leo28hotmail.com <roger_leo28@hotmail.com>
2026-07-15 20:30:53 -0600

    8.3: Todos los autores -Completado

--- library-backend/index.js
+++ library-backend/index.js
@@ -82,17 +82,30 @@ const typeDefs = `
     id: ID!
   }
 
+  type Author {
+    name: String!
+    born: Int
+    bookCount: Int!
+    id: ID!
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `;
+
 const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: () => books,
+    allAuthors: () => authors,
+  },
+  Author: {
+    bookCount: (root) => books.filter((b) => b.author === root.name).length,
   },
 };
 
