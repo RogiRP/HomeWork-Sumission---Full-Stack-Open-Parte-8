7d8c10cb148d2a17a71d5ebb8dca4902e0099fd6
roger_leo28hotmail.com <roger_leo28@hotmail.com>
2026-08-02 15:04:05 -0600

    8.25: Suscripciones - cliente, parte 2 -Completado

--- library-frontend/src/App.jsx
+++ library-frontend/src/App.jsx
@@ -1,20 +1,45 @@
-import { useState, useEffect } from 'react'
-import { useSubscription } from '@apollo/client'
+import { useState } from 'react'
+import { useSubscription, useApolloClient } from '@apollo/client'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
 import LoginForm from './components/LoginForm'
 import Recommended from './components/Recommended'
 import { BOOK_ADDED } from './subscriptions'
+import { gql } from '@apollo/client'
+
+const ALL_BOOKS = gql`
+  query allBooks($genre: String) {
+    allBooks(genre: $genre) {
+      title
+      author {
+        name
+      }
+      published
+      id
+    }
+  }
+`
 
 const App = () => {
   const [page, setPage] = useState('authors')
   const [token, setToken] = useState(() => localStorage.getItem('library-user-token'))
+  const client = useApolloClient()
 
   useSubscription(BOOK_ADDED, {
     onData: ({ data }) => {
       const book = data.data.bookAdded
       window.alert(`New book added: ${book.title} by ${book.author.name}`)
+
+      client.cache.updateQuery(
+        { query: ALL_BOOKS, variables: { genre: null } },
+        (existingData) => {
+          if (!existingData) return existingData
+          const alreadyExists = existingData.allBooks.some(b => b.id === book.id)
+          if (alreadyExists) return existingData
+          return { allBooks: existingData.allBooks.concat(book) }
+        }
+      )
     }
   })
 
