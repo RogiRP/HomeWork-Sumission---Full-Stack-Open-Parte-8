3400239247f4f0dbc0e15f9436fb1b8281c989fb
roger_leo28hotmail.com <roger_leo28@hotmail.com>
2026-08-02 13:49:53 -0600

    8.11: Año de nacimiento del autor -Completado

--- library-frontend/src/components/Authors.jsx
+++ library-frontend/src/components/Authors.jsx
@@ -1,4 +1,5 @@
-import { useQuery, gql } from '@apollo/client'
+import { useQuery, useMutation, gql } from '@apollo/client'
+import { useState } from 'react'
 
 const ALL_AUTHORS = gql`
   query {
@@ -11,13 +12,40 @@ const ALL_AUTHORS = gql`
   }
 `
 
+const EDIT_AUTHOR = gql`
+  mutation editAuthor($name: String!, $setBornTo: Int!) {
+    editAuthor(name: $name, setBornTo: $setBornTo) {
+      name
+      born
+      id
+    }
+  }
+`
+
 const Authors = (props) => {
   const result = useQuery(ALL_AUTHORS)
+  const [editAuthor] = useMutation(EDIT_AUTHOR, {
+    refetchQueries: [{ query: ALL_AUTHORS }]
+  })
+
+  const [name, setName] = useState('')
+  const [born, setBorn] = useState('')
 
   if (!props.show) return null
   if (result.loading) return <div>loading...</div>
 
   const authors = result.data.allAuthors
+
+  const submit = async (event) => {
+    event.preventDefault()
+    const bornYear = parseInt(born)
+    if (isNaN(bornYear) || bornYear < 1000 || bornYear > new Date().getFullYear()) {
+      alert('Please enter a valid birth year')
+      return
+    }
+    await editAuthor({ variables: { name, setBornTo: bornYear } })
+    setBorn('')
+  }
 
   return (
     <div>
@@ -38,6 +66,30 @@ const Authors = (props) => {
           ))}
         </tbody>
       </table>
+
+      <h3>Set birthyear</h3>
+      <form onSubmit={submit}>
+        <div>
+          name
+          <select value={name} onChange={({ target }) => setName(target.value)}>
+            <option value="">select author</option>
+            {authors.map((a) => (
+              <option key={a.id} value={a.name}>{a.name}</option>
+            ))}
+          </select>
+        </div>
+        <div>
+          born
+          <input
+            type="number"
+            value={born}
+            onChange={({ target }) => setBorn(target.value)}
+            min="1000"
+            max={new Date().getFullYear()}
+          />
+        </div>
+        <button type="submit">update author</button>
+      </form>
     </div>
   )
 }
